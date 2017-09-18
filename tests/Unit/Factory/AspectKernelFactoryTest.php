24b7bb6af738419b1067731f015856a2b90702fb
Martin Reinfandt <reinfandt@z-em.de>
2017-09-18 12:42:24 +0200

    add aspect kernel factory unit test

---
+++ tests/Unit/Factory/AspectKernelFactoryTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Go\ZF2\GoAopModule\Tests\Unit\Factory;
+
+use Go\Core\AspectKernel;
+use Go\ZF2\GoAopModule\Factory\AspectKernelFactory;
+use PHPUnit\Framework\TestCase;
+use Zend\ServiceManager\ServiceLocatorInterface;
+
+/**
+ * @package Go\ZF2\GoAopModule\Tests\Unit\Factory
+ */
+class AspectKernelFactoryTest extends TestCase
+{
+    const CONFIG_PATH = __DIR__ . '/../../resources/goaop_module.php';
+
+    /**
+     * @test
+     */
+    public function itCreatesKernelOnInvoke()
+    {
+        $serviceLocator = $this->prophesize(ServiceLocatorInterface::class);
+        $serviceLocator->get('config')
+            ->willReturn(['goaop_module' => require static::CONFIG_PATH])
+            ->shouldBeCalled();
+
+        $factory = new AspectKernelFactory();
+
+        $instance = $factory($serviceLocator->reveal(), AspectKernel::class);
+
+        $this->assertInstanceOf(
+            AspectKernel::class,
+            $instance,
+            'factory should return an instance of ' . AspectKernel::class
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function itCreatesKernelOnCreateService()
+    {
+        $serviceLocator = $this->prophesize(ServiceLocatorInterface::class);
+        $serviceLocator->get('config')
+            ->willReturn(['goaop_module' => require static::CONFIG_PATH])
+            ->shouldBeCalled();
+
+        $factory = new AspectKernelFactory();
+
+        $instance = $factory->createService($serviceLocator->reveal());
+
+        $this->assertInstanceOf(
+            AspectKernel::class,
+            $instance,
+            'factory should return an instance of ' . AspectKernel::class
+        );
+    }
+}
